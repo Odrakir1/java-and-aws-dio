f16592a377c37fc759143f210fcdcce1ab1e481f
Ricardo Moura <rikardomoura1@gmail.com>
2021-09-12 15:14:01 -0300

    Add validation to Jedi entity and add feedback operations

--- src/main/java/br/com/ricardo/spring/web/mvc/controller/JediController.java
+++ src/main/java/br/com/ricardo/spring/web/mvc/controller/JediController.java
@@ -1,12 +1,16 @@
 package br.com.ricardo.spring.web.mvc.controller;
 
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.ricardo.spring.web.mvc.model.Jedi;
 import br.com.ricardo.spring.web.mvc.repository.JediRepository;
@@ -40,8 +44,14 @@ public class JediController {
 	}
 	
 	@PostMapping("/jedi")
-	public String createJedi(@ModelAttribute Jedi jedi) {
+	public String createJedi(@Valid @ModelAttribute Jedi jedi, BindingResult result, RedirectAttributes redirectAttributes) {
+		
+		if(result.hasErrors())
+			return "new-jedi";
+		
 		jediRepository.add(jedi);
+		
+		redirectAttributes.addFlashAttribute("message", "Jedi was successfully added.");
 		
 		return "redirect:jedi";
 	}
