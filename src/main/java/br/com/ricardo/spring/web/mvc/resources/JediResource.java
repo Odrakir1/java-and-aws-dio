52c7cc716ac4eebece686247a3018eb23be7a1b5
Ricardo Moura <rikardomoura1@gmail.com>
2021-09-12 17:11:14 -0300

    Respond with correct HTTP code when posting a new Jedi

--- src/main/java/br/com/ricardo/spring/web/mvc/resources/JediResource.java
+++ src/main/java/br/com/ricardo/spring/web/mvc/resources/JediResource.java
@@ -6,12 +6,14 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ricardo.spring.web.mvc.model.Jedi;
@@ -40,6 +42,7 @@ public class JediResource {
 	}
 	
 	@PostMapping("/api/jedi")
+	@ResponseStatus(HttpStatus.CREATED)
 	public Jedi createJedi(@Valid @RequestBody Jedi jedi){
 		return jediRepository.add(jedi);
 		
